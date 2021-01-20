d55987f9cce1b18dbdc48bd92437d91afeb51001
Chris Marchesi <chrism@vancluevertech.com>
2021-01-20 11:45:12 -0800

    WIP - final before rebase

--- src/execute.js
+++ src/execute.js
@@ -23,188 +23,31 @@ function evaluateOpCodes(context, opCodes, callback) {
         break
 
       case 'pick':
-        context = context.reduce((result, each) => {
-          if (each != null && typeof each !== 'object') {
-            if (opCode.strict) {
-              throw new Error(`Cannot index ${typeof each} with ${opCode.key}`)
-            }
-            // Skip this value entirely
-            return result
-          }
-          let picked = each[opCode.key]
-          result.push(picked)
-          return result
-        }, [])
+        context = evaluateOpCode_pick(context, opCode)
         break
 
       case 'index':
-        context = context.reduce((result, each) => {
-          if (!Array.isArray(each)) {
-            if (opCode.strict) {
-              throw new Error('Can only index into arrays')
-            }
-            return result
-          }
-          let indexed
-          if (Math.abs(opCode.index) > each.length || opCode.index === each.length) {
-            indexed = null
-          } else if (opCode.index < 0) {
-            indexed = each.slice(opCode.index)[0]
-          } else {
-            indexed = each[opCode.index]
-          }
-          result.push(indexed)
-          return result
-        }, [])
+        context = evaluateOpCode_index(context, opCode)
         break
 
       case 'slice':
-        context = context.reduce((result, each) => {
-          if ('undefined' === typeof each.slice) {
-            if (opCode.strict) {
-              throw new Error('Cannot slice ' + typeof each)
-            }
-            return result
-          }
-          if (undefined === opCode.start && undefined === opCode.end) {
-            throw new Error('Cannot slice with no offsets')
-          }
-          result.push(each.slice(opCode.start, opCode.end))
-          return result
-        }, [])
+        context = evaluateOpCode_slice(context, opCode)
         break
 
       case 'explode':
-        context = context.reduce((result, each) => {
-          if (Array.isArray(each)) {
-            return result.concat(each)
-          } else if (typeof each === 'object' && each != null) {
-            return result.concat(Object.values(each))
-          }
-
-          if (opCode.strict) {
-            let type = typeof each
-            if (each === null) {
-              // jq throws an error specifically for null, so let's
-              // distinguish that from object.
-              type = 'null'
-            }
-            throw new Error('Cannot iterate over ' + type)
-          }
-          return result
-        }, [])
-        if (callback) {
-          callback(context.length)
-        }
+        context = evaluateOpCode_explode(context, opCode, callback)
         break
 
       case 'create_array':
-        context = [ opCode.values.reduce((result, each) => { 
-          let exploded = false
-          let explodedLen = 0
-          function cb(len) {
-            exploded = true
-            explodedLen = len
-            if (callback) {
-              callback(len)
-            }
-          }
-          values = evaluateOpCodes(context, [...each], cb)
-          if (!exploded) {
-            result.push(values)
-          } else {
-            switch (explodedLen) {
-              case 0:
-                break
-               
-              case 1:
-                result.push(values)
-                break
-
-              default:
-                result = result.concat(values)
-            }
-          }
-
-          return result
-        }, []) ]
+        context = evaluateOpCode_create_array(context, opCode, callback)
         break
 
       case 'create_object':
-        function buildObject(current, ops) {
-          let result = []
-          let key, values
-          [key, values] = ops.shift()
-          values.forEach(value => {
-            current[key] = value
-            if (ops.length > 0) {
-              result = result.concat(buildObject({...current}, [...ops]))
-            } else {
-              result.push({...current})
-            }
-          })
-
-          return result
-        }
-
-        context = buildObject({}, Object.entries(opCode.entries.reduce((result, each) => {
-          let exploded = false
-          let explodedLen = 0
-          function cb(len) {
-            exploded = true
-            explodedLen = len
-            if (callback) {
-              callback(len)
-            }
-          }
-          let values = evaluateOpCodes(context, [...each.value], cb)
-          if (!exploded) {
-            result[each.key] = [values]
-          } else {
-            switch (explodedLen) {
-              case 0:
-                result[each.key] = []
-                break
-              
-              case 1:
-                result[each.key] = [values]
-                break
-
-              default:
-                result[each.key] = values
-                break
-            }
-          }
-
-          return result
-        }, {})))
+        context = evaluateOpCode_create_object(context, opCode, callback)
         break
 
       case 'pipe':
-        let exploded = false
-        let explodedLen = 0
-        function cb(len) {
-          exploded = true
-          explodedLen = len
-          if (callback) {
-            callback(len)
-          }
-        }
-        context = evaluateOpCodes(context, [...opCode.in], cb)
-        if (!exploded) {
-          context = evaluateOpCodes([context], [...opCode.out])
-        } else {
-          switch (explodedLen) {
-            case 0:
-              context = []
-              break
-            
-            case 1:
-              context = [context]
-              break
-          }
-          context = context.map(each => evaluateOpCodes([each], [...opCode.out]))
-        }
+        context = evaluateOpCode_pipe(context, opCode, callback)
         break
 
       default:
@@ -213,4 +56,185 @@ function evaluateOpCodes(context, opCodes, callback) {
   } while (opCodes.length > 0)
 
   return context.length > 1 ? context : context[0]
+}
+
+function evaluateOpCode_pick(context, opCode) {
+  return context.reduce((result, each) => {
+    if (each != null && typeof each !== 'object') {
+      if (opCode.strict) {
+        throw new Error(`Cannot index ${typeof each} with ${opCode.key}`)
+      }
+      // Skip this value entirely
+      return result
+    }
+    let picked = each[opCode.key]
+    result.push(picked)
+    return result
+  }, [])
+}
+
+function evaluateOpCode_index(context, opCode) {
+  return context.reduce((result, each) => {
+    if (!Array.isArray(each)) {
+      if (opCode.strict) {
+        throw new Error('Can only index into arrays')
+      }
+      return result
+    }
+    let indexed
+    if (Math.abs(opCode.index) > each.length || opCode.index === each.length) {
+      indexed = null
+    } else if (opCode.index < 0) {
+      indexed = each.slice(opCode.index)[0]
+    } else {
+      indexed = each[opCode.index]
+    }
+    result.push(indexed)
+    return result
+  }, [])
+}
+
+function evaluateOpCode_slice(context, opCode) {
+  return context.reduce((result, each) => {
+    if ('undefined' === typeof each.slice) {
+      if (opCode.strict) {
+        throw new Error('Cannot slice ' + typeof each)
+      }
+      return result
+    }
+    if (undefined === opCode.start && undefined === opCode.end) {
+      throw new Error('Cannot slice with no offsets')
+    }
+    result.push(each.slice(opCode.start, opCode.end))
+    return result
+  }, [])
+}
+
+function evaluateOpCode_explode(context, opCode, callback) {
+  context = context.reduce((result, each) => {
+    if (Array.isArray(each)) {
+      return result.concat(each)
+    } else if (typeof each === 'object' && each != null) {
+      return result.concat(Object.values(each))
+    }
+    if (opCode.strict) {
+      let type = typeof each
+      if (each === null) {
+        // jq throws an error specifically for null, so let's
+        // distinguish that from object.
+        type = 'null'
+      }
+      throw new Error('Cannot iterate over ' + type)
+    }
+    return result
+  }, [])
+  if (callback) {
+    callback(context.length)
+  }
+  return context
+}
+
+function evaluateOpCode_create_array(context, opCode, callback) {
+  return [ opCode.values.reduce((result, each) => { 
+    // let exploded = false
+    // let explodedLen = 0
+    // const cb = makeCb(exploded, explodedLen, callback)
+    let exploder, cb 
+    [exploder, cb] = makeExploder(callback)
+    let values = evaluateOpCodes(context, [...each], cb)
+    if (!exploder.exploded) {
+      result.push(values)
+    } else {
+      switch (exploder.length) {
+        case 0:
+          break
+         
+        case 1:
+          result.push(values)
+          break
+
+        default:
+          result = result.concat(values)
+      }
+    }
+
+    return result
+  }, []) ]
+}
+
+function evaluateOpCode_create_object(context, opCode, callback) {
+  return evaluateOpCode_create_object_build({}, Object.entries(opCode.entries.reduce((result, each) => {
+    let exploder, cb 
+    [exploder, cb] = makeExploder(callback)
+    let values = evaluateOpCodes(context, [...each.value], cb)
+    if (!exploder.exploded) {
+      result[each.key] = [values]
+    } else {
+      switch (exploder.length) {
+        case 0:
+          result[each.key] = []
+          break
+        
+        case 1:
+          result[each.key] = [values]
+          break
+
+        default:
+          result[each.key] = values
+          break
+      }
+    }
+    return result
+  }, {})))
+}
+  
+function evaluateOpCode_create_object_build(current, ops) {
+  let result = []
+  let key, values
+  [key, values] = ops.shift()
+  values.forEach(value => {
+    current[key] = value
+    if (ops.length > 0) {
+      result = result.concat(evaluateOpCode_create_object_build({...current}, [...ops]))
+    } else {
+      result.push({...current})
+    }
+  })
+  return result
+}
+
+function evaluateOpCode_pipe(context, opCode, callback) {
+  let exploder, cb 
+  [exploder, cb] = makeExploder(callback)
+  context = evaluateOpCodes(context, [...opCode.in], cb)
+  if (!exploder.exploded) {
+    context = evaluateOpCodes([context], [...opCode.out])
+  } else {
+    switch (exploder.length) {
+      case 0:
+        context = []
+        break
+      
+      case 1:
+        context = [context]
+        break
+    }
+    context = context.map(each => evaluateOpCodes([each], [...opCode.out]))
+  }
+  return context
+}
+
+function makeExploder(callback) {
+  let exploder = {exploded: false, length: 0}
+  return [exploder, makeExploderCb(exploder, callback)]
+}
+
+function makeExploderCb(exploder, callback) {
+  return (length) => {
+    exploder.exploded = true
+    exploder.length = length
+    if (callback) {
+      callback(length)
+    }
+  }
 }
